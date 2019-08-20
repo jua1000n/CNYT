747aaf904bfbbc1ba982131f6d51189d0007a769
jua1000n <53835422+jua1000n@users.noreply.github.com>
2019-08-20 11:12:41 -0500

    Update Complejos.py

--- Complejos.py
+++ Complejos.py
@@ -1,71 +1,50 @@
 from sys import stdin
 import math
-def prue():
-    r = [2, 3]
-    s = [1, 1]
-    print(prod(r, s))
-    print(suma(r, s))
-    print(prod(r, s))
-    print(res(r, s))
-    print(div(r, s))
-    print(mod(r))
-    print(convercarpol(r))
-    print(fase(r))
-
-
+# Suma de numeros complejos
 def suma(r, i):
-    res1 = r[0] + i[0]
-    res2 = r[1] + i[1]
-    return "{}+{}i".format(res1, res2)
+    res1 = float(r[0]) + float(i[0])
+    res2 = float(r[1]) + float(i[1])
+    return (res1, res2)
 
+#Multiplicacion de numeros complejos
+def producto(r, i):
+    res1 = (r[0] * i[0]) - (r[1] * i[1])
+    res2 = (r[0] * i[1]) + (r[1] * i[0])
+    return (res1, res2)
 
-def prod(r, i):
-    if len(r) == 1:
-        res1 = r[0] * i[0]
-        res2 = r[0] * i[1]
-    elif len(i) == 1:
-        res1 = i[0] * r[0]
-        res2 = i[0] * r[1]
-    elif len(i) == 2 and len(r) == 2:
-        res1 = (r[0] * i[0]) - (r[1] * i[1])
-        res2 = (r[0] * i[1]) + (r[1] * i[0])
-    return "{}+{}i".format(res1, res2)
-
-
+#Resta de numeros complejos
 def res(r, i):
     res1 = r[0] - i[0]
     res2 = r[1] - i[1]
-    return "{}+{}i".format(res1, res2)
+    return (res1, res2)
 
-
+#Division de numeros complejos
 def div(r, i):
-    res1 = ((r[0] * i[0]) + (r[1] * i[1])) / ((i[0] ** 2) + (i[1] ** 2))
-    res2 = ((r[0] * i[1]) - (r[1] * i[0])) / ((i[0] ** 2) + (i[1] ** 2))
-    return "{}+{}i".format(res1, res2)
+    res1 = round(((r[0] * i[0]) + (r[1] * i[1])) / ((i[0] ** 2) + (i[1] ** 2)),4)
+    res2 = round(((r[1] * i[0]) - (r[0] * i[1])) / ((i[0] ** 2) + (i[1] ** 2)),4)
+    return (res1, res2)
 
-
+#modulo
 def mod(r):
-    res1 = ((r[0] ** 2) + (r[1] ** 2)) ** (1 / 2)   
-    return "{}".format(res1)
+    res1 = round(((r[0] ** 2) + (r[1] ** 2)) ** (1 / 2),4)
+    return (res1)
 
-
+#Conjugado
 def conj(i):
     r = [i[0], i[1]]
     res1 = -1 * r[1]
     r[1] = res1
-    return "{}".format(r)
+    p=(r[0],r[1])
+    return p
 
-
+#De cartesiano a polar
 def convercarpol(r):
     p = mod(r)
     teta = fase(r)
-    return "{}, {}".format(p,teta)
+    return (p, teta)
 
+#Retornar la fase de un numero complejo
 def fase(r):
     fa = math.atan(abs(r[1] / r[0]))
     fa = fa * (180 / math.pi)
-    return "{}".format(fa)
-
-
-    
-prue()
+    return round(fa,4)
